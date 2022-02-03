46109f1f1011efcd4f5d8a02d07127d5d62c1bdf
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2022-02-03 21:49:55 +0100

    Prepare configFile section to match current setup

--- config/nixpkgs/xdg.nix
+++ config/nixpkgs/xdg.nix
@@ -19,8 +19,29 @@
       documents = "$HOME/documents";
     };
 
+    /*
     configFile = {
-      # "hm_test".source = ../hm_test;
+      # whole folder as symlink
+      "alacritty".source = ../alacritty;
+      "bash".source = ../bash;
+      "fontconfig".source = ../fontconfig;
+      "git".source = ../git;
+      "gtk-3.0".source = ../gtk-3.0;
+      "imv".source = ../imv;
+      "mpv".source = ../mpv;
+      "nvim".source = ../nvim;
+      "qt5ct".source = ../qt5ct;
+      "tig".source = ../tig;
+      "tmux".source = ../tmux;
+      # only specific files as symlinks
+      "mpd/mpd.conf".source = ../mpd/mpd.conf;
+      "ranger/rc.conf".source = ../ranger/rc.conf;
+      "ranger/rifle.conf".source = ../ranger/rifle.conf;
+      # X11 specific
+      "awesome".source = ../awesome;
+      "picom".source = ../picom;
+      "X11".source = ../X11;
     };
+    */
   };
 }
