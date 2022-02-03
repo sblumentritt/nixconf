507f9cb3430c97ed0ccff9e1fb3c7ffd1ccd513d
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2022-02-03 10:09:14 +0100

    Move packages list to its own nix file

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,62 +1,14 @@
 { config, pkgs, ... }:
 
+let
+  packages = import ./packages.nix;
+in
 {
   # provide required information
   home.username = "sebastian";
   home.homeDirectory = "/home/sebastian";
 
-  home.packages = with pkgs; [
-    # llvm
-    llvmPackages_13.llvm
-    clang_13
-    clang-tools
-    lld_13
-    lldb
-    # development
-    git
-    git-lfs
-    tk
-    cmake
-    cppcheck
-    gdb
-    ninja
-    meld
-    sumneko-lua-language-server
-    # Qt/QML development
-    qtcreator
-    # music
-    mpd
-    mpc-cli
-    # commandline
-    alacritty
-    tmux
-    neovim
-    ranger
-    tig
-    htop
-    fzf
-    ripgrep
-    fd
-    exa
-    jq
-    codespell
-    # style
-    libsForQt5.qt5ct
-    papirus-icon-theme
-    # graphics & design
-    scribusUnstable
-    krita
-    gmic-qt-krita
-    inkscape
-    # browser
-    firefox
-    # other
-    transmission-qt
-    mpv
-    imv
-    mupdf
-    android-file-transfer
-  ];
+  home.packages = packages pkgs;
 
   # determines the home manager release that the configuration is compatible
   home.stateVersion = "22.05";
---
+++ config/nixpkgs/packages.nix
@@ -0,0 +1,52 @@
+pkgs: with pkgs; [
+    # llvm
+    llvmPackages_13.llvm
+    clang_13
+    clang-tools
+    lld_13
+    lldb
+    # development
+    git
+    git-lfs
+    tk
+    cmake
+    cppcheck
+    gdb
+    ninja
+    meld
+    sumneko-lua-language-server
+    # Qt/QML development
+    qtcreator
+    # music
+    mpd
+    mpc-cli
+    # commandline
+    alacritty
+    tmux
+    neovim
+    ranger
+    tig
+    htop
+    fzf
+    ripgrep
+    fd
+    exa
+    jq
+    codespell
+    # style
+    libsForQt5.qt5ct
+    papirus-icon-theme
+    # graphics & design
+    scribusUnstable
+    krita
+    gmic-qt-krita
+    inkscape
+    # browser
+    firefox
+    # other
+    transmission-qt
+    mpv
+    imv
+    mupdf
+    android-file-transfer
+  ]
