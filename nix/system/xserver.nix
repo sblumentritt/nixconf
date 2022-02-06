1cc0fd9d8245907e193afb1461e49153cc87eba7
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2022-02-06 09:19:58 +0100

    Extend xserver.nix with monitor, keyboard and window manager options

--- nix/system/xserver.nix
+++ nix/system/xserver.nix
@@ -8,6 +8,27 @@
 
       # only use a TTY login prompt instead of the default 'lightdm'
       displayManager.startx.enable = true;
+
+      windowManager.awesome = {
+        enable = true;
+        luaModules = [ pkgs.luajitPackages.vicious ];
+      };
+
+      libinput.enable = true;
+
+      xrandrHeads = [
+        {
+          output = "HDMI-A-1";
+        }
+        {
+          output = "HDMI-A-0";
+          primary = true;
+        }
+      ];
+
+      layout = "us,de";
+      xkbModel = "pc105";
+      xkbOptions = "grp:ctrls_toggle,caps:backspace,shift:both_capslock_cancel";
     };
   };
 }
